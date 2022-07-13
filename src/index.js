069fc6bc23e8cb908e2c12eab6d142455aef0ed6
ulis <ycm93dg@gmail.com>
2022-07-14 02:27:34 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+const clockTitle = document.querySelector(".js-clock");
 
-import App from "./App";
+function getClock() {
+  const date = new Date();
+  const thisYear = date.getFullYear();
+  const christmas = new Date(`December 25, ${thisYear} 00:00:00`);
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+  if (christmas.getTime() - date.getTime() < 0) {
+    //already passed this year christmas
+    christmas.setFullYear(thisYear + 1);
+  }
+  const msDiff = christmas.getTime() - date.getTime();
+  const secDiff = parseInt(msDiff / 1000, 0);
+  const secMod = secDiff % 60;
+  const minDiff = parseInt(secDiff / 60, 0);
+  const minMod = minDiff % 60;
+  const hourDiff = parseInt(minDiff / 60, 0);
+  const hourMod = hourDiff % 24;
+  const dateDiff = parseInt(hourDiff / 24, 0);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+  const datesText = String(dateDiff);
+  const hoursText = String(hourMod).padStart(2, "0");
+  const minutesText = String(minMod).padStart(2, "0");
+  const secondsText = String(secMod).padStart(2, "0");
+  clockTitle.innerText = `${datesText}d ${hoursText}h ${minutesText}h ${secondsText}s`;
+}
+getClock();
+setInterval(getClock, 1000);
